6b2ee2e01877e37ee442c3909b82496608b4f4bf
Rafael Khachikian <Khachikyan38@gmail.com>
2022-04-19 00:24:14 +0300

    update import method

--- src/Components/Card/Card.index.jsx
+++ src/Components/Card/Card.index.jsx
@@ -1,19 +1,6 @@
 import { useState } from "react";
 
-import {
-  Box,
-  Equilibrium,
-  Title,
-  SecondaryTxt,
-  EthereumTxt,
-  FlexRowBlock,
-  HumanWrapper,
-  Icon,
-  Devider,
-  Human,
-  Name,
-  Overlay,
-} from "./Card.styles";
+import * as C from "./Card.styles";
 
 import ethereum from "../../images/icon-ethereum.svg";
 import clock from "../../images/icon-clock.svg";
@@ -23,39 +10,36 @@ const Card = () => {
   const [isOverlayVisible, setIsOverlayVisible] = useState(false);
 
   return (
-    <Box>
-      <Equilibrium
+    <C.Box>
+      <C.Equilibrium
         onMouseEnter={() => setIsOverlayVisible(true)}
         onMouseLeave={() => setIsOverlayVisible(false)}
       >
-        {isOverlayVisible && <Overlay />}
-      </Equilibrium>
-      <Title>Equilibrium #3429</Title>
-      <SecondaryTxt>
+        {isOverlayVisible && <C.Overlay />}
+      </C.Equilibrium>
+      <C.Title>Equilibrium #3429</C.Title>
+      <C.SecondaryTxt>
         Our Equilibrium collection promotes balance and calm.
-      </SecondaryTxt>
+      </C.SecondaryTxt>
+      <C.FlexRowBlock>
+        <C.FlexRowBlock>
+          <C.Icon src={ethereum} alt="ethereum" />
+          <C.EthereumTxt>0.041 ETH</C.EthereumTxt>
+        </C.FlexRowBlock>
 
-      <FlexRowBlock>
-        <FlexRowBlock>
-          <Icon src={ethereum} alt="ethereum" />
-          <EthereumTxt>0.041 ETH</EthereumTxt>
-        </FlexRowBlock>
-
-        <FlexRowBlock>
-          <Icon src={clock} primary alt="clock" />
-          <SecondaryTxt>3 days left</SecondaryTxt>
-        </FlexRowBlock>
-      </FlexRowBlock>
-
-      <Devider />
-
-      <HumanWrapper>
-        <Human src={avatar} alt="Jules Wyvern avatar" />
-        <SecondaryTxt>
-          Creation of <Name>Jules Wyvern</Name>
-        </SecondaryTxt>
-      </HumanWrapper>
-    </Box>
+        <C.FlexRowBlock>
+          <C.Icon src={clock} primary alt="clock" />
+          <C.SecondaryTxt>3 days left</C.SecondaryTxt>
+        </C.FlexRowBlock>
+      </C.FlexRowBlock>
+      <C.Devider />
+      <C.HumanWrapper>
+        <C.Human src={avatar} alt="Jules Wyvern avatar" />
+        <C.SecondaryTxt>
+          Creation of <C.Name>Jules Wyvern</C.Name>
+        </C.SecondaryTxt>
+      </C.HumanWrapper>
+    </C.Box>
   );
 };
 
